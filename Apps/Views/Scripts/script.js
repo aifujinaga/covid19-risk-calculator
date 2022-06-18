c74c7f4ab7df88a315de6f5c5f707f29f5fe341d
Masahiro noma <macharo.0316@gmail.com>
2022-06-18 09:19:13 +0900

    感染者数の計算修正

--- Apps/Views/Scripts/script.js
+++ Apps/Views/Scripts/script.js
@@ -7,6 +7,7 @@ const aerosolRiskElement = document.getElementById("aerosolRisk");
 const contactRiskElement = document.getElementById("contactRisk");
 const isValidButtionElement = document.getElementById("isvalid-button");
 const modalTriggerElement = document.getElementById("modal-trigger");
+const towWeekDay = 14
 
 async function callApi() {
     aria = ariaSelect.value;
@@ -15,20 +16,14 @@ async function callApi() {
         return;
     }
 
-    url = "https://services.arcgis.com/wlVTGRSYTzAbjjiC/arcgis/rest/services/all_patient_v2_prefecture_point_view/FeatureServer/0/query?where=Name='" + aria + "'&outFields=人口10万人あたり感染者数&outSR=4326&f=json"
+    url = "https://services.arcgis.com/wlVTGRSYTzAbjjiC/arcgis/rest/services/all_patient_v2_prefecture_point_view/FeatureServer/0/query?where=Name='" + aria + "'&outFields=人口10万人あたり感染者数,&outSR=4326&f=json"
     const res = await fetch(url);
     const resjson = await res.json();
-
-    let infectionCount = 0;
-    let count = 7
-
+    let twoWeekInfectionCount = 0;
     let len = resjson.features.length;
-    for (var index = len - 1; index > len - 1 - count; --index) {
-        infectionCount += resjson.features[index].attributes.人口10万人あたり感染者数;
-    };
-    infectionCount /= count;
-
-    document.getElementById('infectedPeopleId').value = Math.round(infectionCount, 0) + " 人";
+    let index = len - 1;
+    twoWeekInfectionCount = resjson.features[len-1].attributes.人口10万人あたり感染者数 - resjson.features[len - 15].attributes.人口10万人あたり感染者数;
+    document.getElementById('infectedPeopleId').value = Math.round(twoWeekInfectionCount, 0) + " 人";
 };
 
 function calcRisk() {
@@ -41,7 +36,7 @@ function calcRisk() {
     disinfection = parseFloat(formElement.disinfection.value);
     contactRate = parseFloat(formElement.contactRate.value);
 
-    contactProbability = infectedPeople / 1000000 * 2 * contactCountPerDay;
+    contactProbability = infectedPeople / 10**5 * 2 * contactCountPerDay;
     aerosolRisk = maskType * distance * ventilation;
     contactRisk = handWash * disinfection * contactRate;
     infectionRisk = contactProbability * (aerosolRisk + contactRisk) * 100;
